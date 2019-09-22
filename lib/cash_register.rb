2689468b85e11ce6ddf2ae87b6303d1171fad6d3
Steven Laff <steve.laff@gmail.com>
2019-09-22 12:27:10 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -13,7 +13,7 @@ class CashRegister
     self.add_to_price(product_price, product_qty)
   end
   
-  def self.add_to_price(price, qty = 1)
+  def add_to_price(price, qty = 1)
     @total += (price * qty)
   end
   
