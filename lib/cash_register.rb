861dee607eee36dac99bbc7266afc331fe8a185e
Steven Laff <steve.laff@gmail.com>
2019-09-22 12:05:30 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,29 @@
+class CashRegister
+  
+  attr_accessor :total, :items, :discount
+  
+  def initialize(discount = 0)
+    @total = 0.0
+    @discount = discount
+    @items = []
+  end
+  
+  def add_item(product_name, product_price, product_qty = 1)
+    @items << {product: product_name, product_price, product_qty}
+  end
+  
+  def apply_discount
+    if @discount > 0
+      @discount = @discount/100.to_f
+      @total = @total - (@total * @discount)
+      
+    else
+      "There is no discount to apply."
+    end
+  end
+  
+end
+
+
+
 
